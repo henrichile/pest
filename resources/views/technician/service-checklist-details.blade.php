f85db84a029d0135be0a359aa39831884313a048
Enrique Tapia <e.tapia46@alumnos.santotomas.cl>
2025-11-24 09:45:07 -0300

    fix: ajustar tamaño del ícono en las observaciones de la lista de chequeo

--- resources/views/technician/service-checklist-details.blade.php
+++ resources/views/technician/service-checklist-details.blade.php
@@ -197,7 +197,7 @@
                                     <ul class="mt-2 space-y-1">
                                         @foreach($station['observations'] as $obs)
                                         <li class="flex items-center text-gray-700 text-sm">
-                                            <svg class="w-3 h-3 text-yellow-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                                            <svg width="20" height="20" class="text-yellow-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
                                                 <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
                                             </svg>
                                             {{ ucfirst(str_replace('_', ' ', $obs)) }}
