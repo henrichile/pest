0cff2c86643bc112094c2a8c1fad74b6e8c984e7
enrique tapia <e.tapia46@alumnos.santotomas.cl>
2025-11-28 18:18:26 +0000

    fix: Remover SoftDeletes del modelo Client ya que la tabla no tiene deleted_at

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 class Client extends Model
 {
-    use HasFactory, SoftDeletes, LogsActivity;
+    use HasFactory, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
