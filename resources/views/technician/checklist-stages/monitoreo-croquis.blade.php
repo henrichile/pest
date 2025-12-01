dfb5f0ccce0fe14c322cca9c0f938e7a9cc9de37
Enrique Tapia <e.tapia46@alumnos.santotomas.cl>
2025-11-30 21:52:49 -0300

    fix: correct technician view service checklist submission route name in technician view

--- resources/views/technician/checklist-stages/monitoreo-croquis.blade.php
+++ resources/views/technician/checklist-stages/monitoreo-croquis.blade.php
@@ -2,7 +2,7 @@
 $isViewingAsTechnician = (session('view_as_technician', false) && auth()->check() && auth()->user()->hasRole('super-admin')) 
     || request()->is('admin/technician-view/*')
     || (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], '/admin/technician-view/') !== false);
-$submitRoute = $isViewingAsTechnician ? route('technician-view.service.checklist.submit', $service) : route('technician.service.checklist.submit', $service);
+$submitRoute = $isViewingAsTechnician ? route('admin.technician-view.service.checklist.submit', $service) : route('technician.service.checklist.submit', $service);
 @endphp
 <form method="POST" action="{{ $submitRoute }}" data-stage="monitoreo-croquis" id="croquisForm">
     @csrf
