0c6ba323d6d0a3ec4430654acbf5cc9ce9b58093
Enrique Tapia <e.tapia46@alumnos.santotomas.cl>
2025-12-18 20:59:53 -0300

    style: Remove redundant `text-white` class from create service button.

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -13,7 +13,7 @@
         </div>
         @can("create-services")
         <a href="{{ route("admin.services.create") }}" 
-           class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg flex items-center space-x-2 transition-colors dark:text-white dark:bg-green-600 dark:hover:bg-green-700">
+           class="bg-green-600 hover:bg-green-700 px-4 py-2 rounded-lg flex items-center space-x-2 transition-colors dark:text-white dark:bg-green-600 dark:hover:bg-green-700">
             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd"></path>
             </svg>
