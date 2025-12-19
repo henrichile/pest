0a675fb5e615c3bc357d6bbd6df3043f7d88cb12
Enrique Tapia <e.tapia46@alumnos.santotomas.cl>
2025-12-18 22:23:35 -0300

    feat: Adjust dark mode hover background color for user table rows.

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -172,9 +172,9 @@
                         </th>
                     </tr>
                 </thead>
-                <tbody class="bg-white dark:bg-white divide-y divide-gray-200 dark:divide-gray-700">
+                <tbody class="bg-white dark:bg-white divide-y divide-gray-200 dark:divide-gray-700 ">
                     @forelse($users as $user)
-                        <tr class="hover:bg-gray-50 dark:hover:bg-gray-100">
+                        <tr class="hover:bg-gray-50 dark:hover:bg-gray-20">
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <div class="flex items-center">
                                     <div class="flex-shrink-0 h-10 w-10">
