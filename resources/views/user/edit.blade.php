ecd0838df239c4080bb33ab46ac941dece73be56
Enrique Tapia <e.tapia46@alumnos.santotomas.cl>
2025-12-18 22:36:52 -0300

    style: update submit button border color in user edit form.

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -234,7 +234,7 @@
                 <a href="{{ route('admin.users.show', $user) }}" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-lg shadow-sm text-sm font-medium dark:text-white transition-colors">
                     Cancelar
                 </a>
-                <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent rounded-lg shadow-sm text-sm font-medium dark:text-white transition-colors dark:border-green-600 dark:hover:border-green-700 dark:focus:ring-green-500 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-500" >
+                <button type="submit" class="inline-flex items-center px-4 py-2 border border-green-600 rounded-lg shadow-sm text-sm font-medium dark:text-white transition-colors" >
                     <svg class="w-5 h-5 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                     </svg>
