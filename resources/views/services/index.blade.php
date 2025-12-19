322e773092291348eeb2ceb3dcc1a77e36e8e20a
Enrique Tapia <e.tapia46@alumnos.santotomas.cl>
2025-12-18 21:00:32 -0300

    fix: update "Nuevo Servicio" text to "Nuevo Servicio a".

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -17,7 +17,7 @@
             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd"></path>
             </svg>
-            <span>Nuevo Servicio</span>
+            <span>Nuevo Servicio a</span>
         </a>
         @endcan
     </div>
